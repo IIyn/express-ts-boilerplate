22677316461ae4c0abf0fda97b78e8016de91303
yassin <yassinam.pro@gmail.com>
2024-04-10 10:40:07 +0200

    added swagger jsdoc initialization

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,42 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import env from "@/config/env";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const app = express();
 const { PORT, HOST } = env;
+
+// swagger docs
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express-TS-API",
+    version: "1.0.0",
+    description: "Express and Typescript API",
+  },
+  servers: [
+    {
+      url: `http://${HOST}:${PORT}`,
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearer: "JWT",
+      },
+    },
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["src/infrastructure/web/routes/*.ts"],
+};
+const swaggerSpec = swaggerJsDoc(options);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
